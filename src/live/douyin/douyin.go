6bdc6b462b97ce72bab77924378e09d343354ca7
Ben-Huang <824451333@qq.com>
2024-11-29 17:29:16 +0800

    Update douyin.go
    fix douyin api

--- src/live/douyin/douyin.go
+++ src/live/douyin/douyin.go
@@ -2,19 +2,17 @@ package douyin
 
 import (
 	"fmt"
-	"net/http"
-	"net/url"
-	"regexp"
-	"sort"
-	"strconv"
-	"strings"
-
-	"github.com/hr3lxphr6j/requests"
-	"github.com/tidwall/gjson"
-
 	"github.com/hr3lxphr6j/bililive-go/src/live"
 	"github.com/hr3lxphr6j/bililive-go/src/live/internal"
 	"github.com/hr3lxphr6j/bililive-go/src/pkg/utils"
+	"github.com/hr3lxphr6j/requests"
+	"github.com/tidwall/gjson"
+	"net/http"
+	"net/url"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const (
@@ -28,6 +26,7 @@ const (
 )
 
 var roomInfoApiForSprintf = "https://live.douyin.com/webcast/room/web/enter/?aid=6383&app_name=douyin_web&live_id=1&device_platform=web&language=zh-CN&browser_language=zh-CN&browser_platform=Win32&browser_name=Chrome&browser_version=116.0.0.0&web_rid=%s"
+var ttwid = ""
 
 func init() {
 	live.Register(domain, new(builder))
@@ -39,7 +38,6 @@ func (b *builder) Build(url *url.URL, opt ...live.Option) (live.Live, error) {
 	return &Live{
 		BaseLive:        internal.NewBaseLive(url, opt...),
 		responseCookies: make(map[string]string),
-		isUsingLegacy:   false,
 	}, nil
 }
 
@@ -53,357 +51,208 @@ func createRandomOdintt() string {
 
 type Live struct {
 	internal.BaseLive
+	roomID                      string
 	responseCookies             map[string]string
 	LastAvailableStringUrlInfos []live.StreamUrlInfo
-	isUsingLegacy               bool
 }
 
-func (l *Live) getLiveRoomWebPageResponse() (body string, err error) {
+func (l *Live) parseRoomId() error {
+	path := strings.Split(strings.ToLower(l.Url.Path), "/")
+	if len(path) == 0 {
+		return fmt.Errorf("failed to parse room id from url: %s", l.Url.String())
+	}
+	roomID := path[len(path)-1]
+	if roomID[0] == '+' {
+		roomID = roomID[1:]
+	}
+	l.roomID = roomID
+	return nil
+}
+
+func (l *Live) getTtwid() (string, error) {
+	if ttwid != "" {
+		return ttwid, nil
+	}
 	cookies := l.Options.Cookies.Cookies(l.Url)
 	cookieKVs := make(map[string]string)
-	cookieKVs["__ac_nonce"] = createRandomCookie()
-	for key, value := range l.responseCookies {
-		cookieKVs[key] = value
-	}
 	for _, item := range cookies {
 		cookieKVs[item.Name] = item.Value
 	}
-
-	// proxy, _ := url.Parse("http://localhost:8888")
-	requestSession := requests.NewSession(&http.Client{
-		// Transport: &http.Transport{
-		// 	Proxy: http.ProxyURL(proxy),
-		// },
-	})
-	req, err := requests.NewRequest(
-		http.MethodGet,
-		l.Url.String(),
+	for key, value := range l.responseCookies {
+		cookieKVs[key] = value
+	}
+	resp, err := requests.Get(
+		"https://live.douyin.com/1-2-3-4-5-6-7-8-9-0",
 		live.CommonUserAgent,
 		requests.Cookies(cookieKVs),
-		requests.Headers(map[string]interface{}{
-			"Cache-Control": "no-cache",
-		}),
 	)
 	if err != nil {
-		return
-	}
-	cookieWithOdinTt := fmt.Sprintf("odin_tt=%s; %s", createRandomOdintt(), req.Header.Get("Cookie"))
-	req.Header.Set("Cookie", cookieWithOdinTt)
-	resp, err := requestSession.Do(req)
-	if err != nil {
-		return
+		return "", err
 	}
 	switch code := resp.StatusCode; code {
 	case http.StatusOK:
 		for _, cookie := range resp.Cookies() {
-			l.responseCookies[cookie.Name] = cookie.Value
+			if cookie.Name == "ttwid" {
+				ttwid = cookie.Value
+				return ttwid, nil
+			}
 		}
 	default:
-		err = fmt.Errorf("failed to get page, code: %v, %w", code, live.ErrInternalError)
-		return
+		return "", fmt.Errorf("failed to get page, code: %v, %w", code, live.ErrInternalError)
 	}
-	body, err = resp.Text()
-	return
+	return "", fmt.Errorf("failed to get ttwid")
 }
 
-func getMainInfoLine(body string) (json *gjson.Result, err error) {
-	reg, err := regexp.Compile(mainInfoLineCatcherRegex)
-	if err != nil {
-		return
-	}
-	match := reg.FindAllStringSubmatch(body, -1)
-	if match == nil {
-		err = fmt.Errorf("0 match for mainInfoLineCatcherRegex: %s", mainInfoLineCatcherRegex)
-		return
-	}
-	for _, item := range match {
-		if len(item) < 2 {
-			// err = fmt.Errorf("len(item) = %d", len(item))
-			continue
-		}
-		mainInfoLine := item[1]
-
-		// 获取房间信息
-		mainJson := gjson.Parse(fmt.Sprintf(`"%s"`, mainInfoLine))
-		if !mainJson.Exists() {
-			// err = fmt.Errorf(errorMessageForErrorf+". Invalid json: %s", stepNumberForLog, mainInfoLine)
-			continue
-		}
-
-		mainJson = gjson.Parse(mainJson.String()).Get("3")
-		if !mainJson.Exists() {
-			// err = fmt.Errorf(errorMessageForErrorf+". Main json does not exist: %s", stepNumberForLog, mainInfoLine)
-			continue
-		}
-
-		if mainJson.Get("state.roomStore.roomInfo.room.status_str").Exists() {
-			json = &mainJson
+func (l *Live) GetData() (info *live.Info, streamUrlInfos []live.StreamUrlInfo, err error) {
+	if l.roomID == "" {
+		err = l.parseRoomId()
+		if err != nil {
 			return
 		}
 	}
-	return nil, fmt.Errorf("MainInfoLine not found")
-}
-
-func (l *Live) getRoomInfoFromBody(body string) (info *live.Info, streamUrlInfos []live.StreamUrlInfo, err error) {
-	const errorMessageForErrorf = "getRoomInfoFromBody() failed on step %d"
-	stepNumberForLog := 1
-	mainJson, err := getMainInfoLine(body)
-	if err != nil {
-		err = fmt.Errorf(errorMessageForErrorf+". %s", stepNumberForLog, err.Error())
-		return
-	}
-
-	isStreaming := mainJson.Get("state.roomStore.roomInfo.room.status_str").String() == "2"
-	info = &live.Info{
-		Live:     l,
-		HostName: mainJson.Get("state.roomStore.roomInfo.anchor.nickname").String(),
-		RoomName: mainJson.Get("state.roomStore.roomInfo.room.title").String(),
-		Status:   isStreaming,
-	}
-	if !isStreaming {
-		return
-	}
-	stepNumberForLog++
-
-	// 获取直播流信息
-	streamIdPath := "state.streamStore.streamData.H264_streamData.common.stream"
-	streamId := mainJson.Get(streamIdPath).String()
-	if streamId == "" {
-		err = fmt.Errorf(errorMessageForErrorf+". %s does not exist", stepNumberForLog, streamIdPath)
-		return
-	}
-	stepNumberForLog++
-
-	streamUrlInfos = make([]live.StreamUrlInfo, 0, 4)
-	reg2, err := regexp.Compile(commonInfoLineCatcherRegex)
-	if err != nil {
-		return
-	}
-	match2 := reg2.FindAllStringSubmatch(body, -1)
-	if match2 == nil {
-		err = fmt.Errorf(errorMessageForErrorf, stepNumberForLog)
-		return
-	}
-	stepNumberForLog++
-
-	for _, item := range match2 {
-		if len(item) < 2 {
-			err = fmt.Errorf(errorMessageForErrorf+". len(item) = %d", stepNumberForLog, len(item))
+	if ttwid, ok := l.responseCookies["ttwid"]; !ok {
+		ttwid, err = l.getTtwid()
+		if err != nil {
 			return
 		}
-		commonJson := gjson.Parse(gjson.Parse(fmt.Sprintf(`"%s"`, item[1])).String())
-		if !commonJson.Exists() {
-			err = fmt.Errorf(errorMessageForErrorf+". Not valid json: %s", stepNumberForLog, item[1])
-			return
-		}
-		if !commonJson.Get("common").Exists() {
-			continue
-		}
-		commonStreamId := commonJson.Get("common.stream").String()
-		if commonStreamId == "" {
-			err = fmt.Errorf(errorMessageForErrorf+". No valid common stream ID: %s", stepNumberForLog, item[1])
-			return
-		}
-		if commonStreamId != streamId {
-			continue
-		}
-
-		commonJson.Get("data").ForEach(func(key, value gjson.Result) bool {
-			flv := value.Get("main.flv").String()
-			var Url *url.URL
-			Url, err = url.Parse(flv)
-			if err != nil {
-				return true
-			}
-			paramsString := value.Get("main.sdk_params").String()
-			paramsJson := gjson.Parse(paramsString)
-			var description strings.Builder
-			paramsJson.ForEach(func(key, value gjson.Result) bool {
-				description.WriteString(key.String())
-				description.WriteString(": ")
-				description.WriteString(value.String())
-				description.WriteString("\n")
-				return true
-			})
-			Resolution := 0
-			resolution := strings.Split(paramsJson.Get("resolution").String(), "x")
-			if len(resolution) == 2 {
-				x, err := strconv.Atoi(resolution[0])
-				if err != nil {
-					return true
-				}
-				y, err := strconv.Atoi(resolution[1])
-				if err != nil {
-					return true
-				}
-				Resolution = x * y
-			}
-			Vbitrate := int(paramsJson.Get("vbitrate").Int())
-			streamUrlInfos = append(streamUrlInfos, live.StreamUrlInfo{
-				Name:        key.String(),
-				Description: description.String(),
-				Url:         Url,
-				Resolution:  Resolution,
-				Vbitrate:    Vbitrate,
-			})
-			return true
-		})
+		l.responseCookies["ttwid"] = ttwid
+		time.Sleep(2 * time.Second)
 	}
-	sort.Slice(streamUrlInfos, func(i, j int) bool {
-		if streamUrlInfos[i].Resolution != streamUrlInfos[j].Resolution {
-			return streamUrlInfos[i].Resolution > streamUrlInfos[j].Resolution
-		} else {
-			return streamUrlInfos[i].Vbitrate > streamUrlInfos[j].Vbitrate
-		}
-	})
-	stepNumberForLog++
-
-	return
-}
-
-func (l *Live) GetInfo() (info *live.Info, err error) {
-	l.isUsingLegacy = false
-	body, err := l.getLiveRoomWebPageResponse()
-	if err != nil {
-		l.LastAvailableStringUrlInfos = nil
-		return
-	}
-
-	var streamUrlInfos []live.StreamUrlInfo
-	info, streamUrlInfos, err = l.getRoomInfoFromBody(body)
-	if err == nil {
-		l.LastAvailableStringUrlInfos = streamUrlInfos
-		return
-	}
-
-	// fallback
-	l.isUsingLegacy = true
-	return l.legacy_GetInfo(body)
-}
-
-func (l *Live) GetStreamUrls() (us []*url.URL, err error) {
-	if !l.isUsingLegacy {
-		if l.LastAvailableStringUrlInfos != nil {
-			us = make([]*url.URL, 0, len(l.LastAvailableStringUrlInfos))
-			for _, urlInfo := range l.LastAvailableStringUrlInfos {
-				us = append(us, urlInfo.Url)
-			}
-			return
-		}
-		return nil, fmt.Errorf("failed douyin GetStreamUrls()")
-	} else {
-		return l.legacy_GetStreamUrls()
-	}
-}
-
-func (l *Live) GetPlatformCNName() string {
-	return cnName
-}
-
-// ================ legacy functions ================
-
-func (l *Live) legacy_getRoomId(body string) (string, error) {
-	roomId := utils.Match1(roomIdCatcherRegex, body)
-	if roomId == "" {
-		return "", fmt.Errorf("failed to get RoomId from page, %w", live.ErrInternalError)
-	}
-	return roomId, nil
-}
-
-func (l *Live) legacy_GetStreamUrls() (us []*url.URL, err error) {
-	var body string
-	body, err = l.getLiveRoomWebPageResponse()
-	if err != nil {
-		l.LastAvailableStringUrlInfos = nil
-		return
-	}
-	data, err := l.legacy_getRoomInfo(body)
-	if err != nil {
-		return nil, err
-	}
-	var urls []string
-	data.Get("data.0.stream_url.flv_pull_url").ForEach(func(key, value gjson.Result) bool {
-		urls = append(urls, value.String())
-		return true
-	})
-	streamData := gjson.Parse(data.Get("data.0.stream_url.live_core_sdk_data.pull_data.stream_data").String())
-	if streamData.Exists() {
-		url := streamData.Get("origin.main.flv")
-		if url.Exists() {
-			urls = append([]string{url.String()}, urls...)
-		}
-	}
-	return utils.GenUrls(urls...)
-}
-
-func (l *Live) legacy_getRoomInfo(body string) (*gjson.Result, error) {
-	roomId, err := l.legacy_getRoomId(body)
-	if err != nil {
-		return nil, err
-	}
-
-	cookies := l.Options.Cookies.Cookies(l.Url)
 	cookieKVs := make(map[string]string)
-	cookieKVs["__ac_nonce"] = createRandomCookie()
+	cookies := l.Options.Cookies.Cookies(l.Url)
 	for _, item := range cookies {
 		cookieKVs[item.Name] = item.Value
 	}
 	for key, value := range l.responseCookies {
 		cookieKVs[key] = value
 	}
-	roomInfoApi := fmt.Sprintf(roomInfoApiForSprintf, roomId)
+	roomInfoApi := fmt.Sprintf(roomInfoApiForSprintf, l.roomID)
 	resp, err := requests.Get(
 		roomInfoApi,
 		live.CommonUserAgent,
 		requests.Cookies(cookieKVs),
-		requests.Headers(map[string]interface{}{
-			"Cache-Control": "no-cache",
-		}))
+	)
 	if err != nil {
-		return nil, err
+		return
 	}
 	switch code := resp.StatusCode; code {
 	case http.StatusOK:
 	case http.StatusNotFound:
-		return nil, live.ErrRoomNotExist
+		return nil, nil, live.ErrRoomNotExist
 	default:
-		return nil, fmt.Errorf("failed to get page, code: %v, %w", code, live.ErrInternalError)
+		return nil, nil, fmt.Errorf("failed to get page, code: %v, %w", code, live.ErrInternalError)
 	}
-
-	body, err = resp.Text()
+	body, err := resp.Text()
 	if err != nil {
-		return nil, err
+		return
 	}
 	result := gjson.Parse(body)
-	return &result, nil
-}
-
-func (l *Live) legacy_GetInfo(body string) (info *live.Info, err error) {
-	data, err := l.legacy_getRoomInfo(body)
-	if err != nil {
-		return nil, err
-	}
-	nickname := data.Get("data.user.nickname").String()
-	title := data.Get("data.data.0.title").String()
-	if title == "" {
-		title = nickname
-	}
-	isLiving := false
-	statusJson := data.Get("data.data.0.status")
-	if statusJson.Exists() {
-		isLiving = statusJson.Int() == 2
-	} else {
-		isLivingJson := data.Get("data.room_status")
-		if !isLivingJson.Exists() {
-			return nil, fmt.Errorf("failed to get room status")
-		}
-		isLiving = isLivingJson.Int() == 0
+	if !result.Get("data").Exists() {
+		return nil, nil, fmt.Errorf("get room info failed")
 	}
 	info = &live.Info{
 		Live:     l,
-		HostName: nickname,
-		RoomName: title,
-		Status:   isLiving,
+		HostName: result.Get("data.user.nickname").String(),
+		RoomName: result.Get("data.data.0.title").String(),
+		Status:   result.Get("data.data.0.status").Int() == 2,
 	}
+	if !info.Status {
+		return info, nil, nil
+	}
+	if !result.Get("data.data.0.stream_url.live_core_sdk_data.pull_data.stream_data").Exists() {
+		return nil, nil, fmt.Errorf("stream data not found")
+	}
+	streamData := gjson.Parse(result.Get("data.data.0.stream_url.live_core_sdk_data.pull_data.stream_data").String())
+	if !streamData.Exists() || !streamData.Get("data").Exists() {
+		return nil, nil, fmt.Errorf("stream data not found")
+	}
+	streamData.Get("data").ForEach(func(key, value gjson.Result) bool {
+		flv := value.Get("main.flv").String()
+		var Url *url.URL
+		Url, err = url.Parse(flv)
+		if err != nil {
+			return true
+		}
+		paramsString := value.Get("main.sdk_params").String()
+		paramsJson := gjson.Parse(paramsString)
+		var description strings.Builder
+		paramsJson.ForEach(func(key, value gjson.Result) bool {
+			description.WriteString(key.String())
+			description.WriteString(": ")
+			description.WriteString(value.String())
+			description.WriteString("\n")
+			return true
+		})
+		Resolution := 0
+		resolution := strings.Split(paramsJson.Get("resolution").String(), "x")
+		if len(resolution) == 2 {
+			x, err := strconv.Atoi(resolution[0])
+			if err != nil {
+				return true
+			}
+			y, err := strconv.Atoi(resolution[1])
+			if err != nil {
+				return true
+			}
+			Resolution = x * y
+		}
+		Vbitrate := int(paramsJson.Get("vbitrate").Int())
+		streamUrlInfos = append(streamUrlInfos, live.StreamUrlInfo{
+			Name:        key.String(),
+			Description: description.String(),
+			Url:         Url,
+			Resolution:  Resolution,
+			Vbitrate:    Vbitrate,
+		})
+		return true
+	})
+
+	// 按清晰度优先级排序, 原画origin 蓝光uhd 超清hd 高清sd 标清ld 流畅md 仅音频ao
+	keyOrder := []string{"origin", "uhd", "hd", "sd", "ld", "md"}
+	sort.Slice(streamUrlInfos, func(i, j int) bool {
+		var index1, index2 = -1, -1
+		for index, name := range keyOrder {
+			if streamUrlInfos[i].Name == name {
+				index1 = index
+			} else if streamUrlInfos[j].Name == name {
+				index2 = index
+			}
+			if index1 != -1 && index2 != -1 {
+				break
+			}
+		}
+		if index1 == -1 {
+			return false
+		}
+		if index2 == -1 {
+			return true
+		}
+		return index1 < index2
+	})
+	return info, streamUrlInfos, nil
+}
+
+func (l *Live) GetInfo() (info *live.Info, err error) {
+	var streamUrlInfos []live.StreamUrlInfo
+	info, streamUrlInfos, err = l.GetData()
+	if err != nil {
+		return
+	}
+	l.LastAvailableStringUrlInfos = streamUrlInfos
 	return
+}
+
+func (l *Live) GetStreamUrls() (us []*url.URL, err error) {
+	if l.LastAvailableStringUrlInfos != nil {
+		us = make([]*url.URL, 0, len(l.LastAvailableStringUrlInfos))
+		for _, urlInfo := range l.LastAvailableStringUrlInfos {
+			us = append(us, urlInfo.Url)
+		}
+		return
+	}
+	return nil, fmt.Errorf("failed douyin GetStreamUrls()")
+}
+
+func (l *Live) GetPlatformCNName() string {
+	return cnName
 }
